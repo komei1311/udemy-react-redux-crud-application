bda230e4136d1ba7937ad8d0ec1e8af4e43d6c0a
bear1311 <bear1211311@gmail.com>
2020-10-25 22:10:51 +0900

    implement events index

---
+++ src/reducers/events.js
@@ -0,0 +1,11 @@
+import _ from 'lodash'
+import { READ_EVENTS } from '../actions'
+
+export default (events = {}, action) => {
+    switch (action.type) {
+        case READ_EVENTS:
+            return _.mapKeys(action.response.data)
+        default:
+            return events 
+    }
+}
